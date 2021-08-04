67872448df0b6f26cce31b1033efa34f9d185994
Ronicson <ronikism@gmail.com>
2021-08-04 12:30:41 -0300

    exercícios repositório

--- ex008.py
+++ ex008.py
@@ -1,6 +1,8 @@
 print('Conversor de metros para centímetros e milímetros')
 m = float(input('Digite o valor em metros?'))
-c = m*100
+km = m / 1000
+cm = m*100
 mm = m*1000
-print(m, 'tem', c, 'centrímetros')
+print(m, 'tem', cm, 'centrímetros')
 print(m, 'tem', mm, 'milímetros')
+print(m, 'tem', km,'Kilômetros')
