8dc8e3ab971a2bb675f4e7eb4068a588d0e9def8
Ronicson <ronikism@gmail.com>
2021-08-09 14:25:29 -0300

    exercícios repositório

--- ex015.py
+++ ex015.py
@@ -2,4 +2,4 @@ km = float(input('Quantos kilometros você percorreu:'))
 d = int(input('Quantos dias você permaneceu com o veiculo:'))
 p1 = d * 60
 p2 = km * 0.15
-print('O valor em diárias é de {}R$ de km é de {:.2f}R$ e total de {:.2f}R$'.format(p1, p2, p1 + p2))
+print('O valor em diárias é de R${} de kms é de R${:.2f} e total de R${:.2f} a pagar.'.format(p1, p2, p1 + p2))
