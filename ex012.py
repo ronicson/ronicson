b9014b258df23769ea0705ecbcb3f07b640dad9f
Ronicson <ronikism@gmail.com>
2021-08-09 12:32:03 -0300

    exercícios repositório

--- ex012.py
+++ ex012.py
@@ -1,4 +1,4 @@
-p = float(input('Digite o valor do produto:'))
-vd = p * 0.05
-print('seu desconto é de {:.2f}'.format(vd))
-print('O novo valor é de {}'.format(p - vd))
+p = float(input('Digite o valor do produto:R$'))
+vd = p - (p * 5/100)
+print('seu desconto é de {:.2f}R$'.format(p - vd))
+print('O novo valor é de {}R$'.format(vd))
--- ex013.py
+++ ex013.py
@@ -1,3 +1,3 @@
 salario = float(input('Digite seu salario:R$'))
-a = salario * 1.15
+a = salario + (15/100)
 print('Seu novo salario é R${:.0f}'.format(a))
