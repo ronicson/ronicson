8f1426f9c2c65620be2a50a195b04673820a4b00
Ronicson <ronikism@gmail.com>
2021-08-09 11:45:52 -0300

    exercícios repositório

---
+++ ex011.py
@@ -0,0 +1,5 @@
+a = float(input('Digite a altura:'))
+l = float(input('Digite a largura:'))
+area = a * l
+t = area/2
+print('A area total para ser pintada é de {}m², a quantidade de tinta necessaria é {}l de tinta'.format(area, t))
