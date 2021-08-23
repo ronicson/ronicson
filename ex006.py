3d3da6da69c147989cebb8190319d4a820296941
Ronicson <ronikism@gmail.com>
2021-08-23 13:49:58 -0300

    exercícios repositório

--- ex006.py
+++ ex006.py
@@ -3,4 +3,4 @@ n = int(input('Digite um número:'))
 db = n * 2
 tp = n * 3
 r2 = n ** (1/2)
-print('O dobro de {} é {}, o triplo é {},e a raiz quadrada é {:.2f}'.format(n, db, tp, r22))
+print('O dobro de {} é {}, o triplo é {},e a raiz quadrada é {:.2f}'.format(n, db, tp, r2))
---
+++ ex017.py
@@ -0,0 +1,4 @@
+ca = float(input('Digite o comprimento do cateto adjacente:'))
+co = float(input('Digite o comprimento do cateto oposto:'))
+ipot = (ca**(1/2)) + (co**(1/2))
+print('O valor da hipotenusa é {}'.format(ipot**2))
