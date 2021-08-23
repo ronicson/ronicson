9ff2c68d877c0130349d3d08e6b8c4ef04f3697e
Ronicson <ronikism@gmail.com>
2021-08-23 16:27:54 -0300

    exercícios repositório

---
+++ ex020.py
@@ -0,0 +1,8 @@
+import random
+a1 = input('Digite se nome:')
+a2 = input('Digite seu nome:')
+a3 = input('Digite seu nome:')
+a4 = input('Digite seu nome:')
+lista = [a1, a2, a3, a4]
+ordem = random.uniform(lista)
+print('A ordem dos trabalos sera assim {}'.format(ordem))
