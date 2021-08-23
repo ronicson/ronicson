af6ac2dbef2ab9f9f457b6b0fb27953c3d245773
Ronicson <ronikism@gmail.com>
2021-08-23 14:45:05 -0300

    exercícios repositório

--- ex017.py
+++ ex017.py
@@ -2,9 +2,8 @@
 co = float(input('Digite o comprimento do cateto oposto:'))
 hipo = (ca**2 + co**2) ** (1/2)
 print('O valor da hipotenusa é {:.2f}'.format(hipo))'''
-import math
+from math import hypot
 ca = float(input('Digite o comprimento do cateto adjacente:'))
 co = float(input('Digite o comprimento do cateto oposto:'))
-hipo = math.hypot(co, ca)
+hipo = hypot(co, ca)
 print('O valor da hipotenusa é {:.2f}'.format(hipo))
-
