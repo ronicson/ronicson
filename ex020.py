74b5738bc7467b7f25451d33e9c16f7a733731ea
Ronicson <ronikism@gmail.com>
2021-08-25 15:17:59 -0300

    exercícios repositório

--- ex020.py
+++ ex020.py
@@ -4,5 +4,5 @@ a2 = input('Digite seu nome:')
 a3 = input('Digite seu nome:')
 a4 = input('Digite seu nome:')
 lista = [a1, a2, a3, a4]
-ordem = random.uniform(lista)
-print('A ordem dos trabalos sera assim {}'.format(ordem))
+random.shuffle(lista)
+print('A ordem dos trabalos sera assim {}'.format(lista))
---
+++ ex021.py
@@ -0,0 +1,4 @@
+import os, time
+os.startfile(r'ex021.mp3')
+time.sleep(10)
+input('Curte o som')
